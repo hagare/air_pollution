287d64184809c8418bc42e5bec22692a668ce182
hagare <helbishlawi@gmail.com>
2015-03-10 11:49:58 -0400

    Uploadiing first draft of code annnd data fiels

---
+++ air_pollution.R
@@ -0,0 +1,7 @@
+pollutantmean=function(directory,pollutant,id=1:332) {
+#directory location of file
+
+#pollutant sulfate or nitrate
+#id location of monitor
+#function returns mean of pollutant across all monitor location 
+}
